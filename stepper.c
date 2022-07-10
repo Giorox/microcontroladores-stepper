e251819a33322f0e1986bb62aae4345daf542745
JVSREco19 <joao.victor.silveira.r@unifei.edu.br>
2022-07-10 16:58:43 -0300

    Adicionando alguns metódos do stepper

--- stepper.c
+++ stepper.c
@@ -1,23 +1,47 @@
 // stepper.c
-#include "lcd.h"
+#include "stepper.h"
 #define _XTAL_FREQ 4000000  // Pass FOSC value to XC8 compiler to enable use of built-in delay functions
+
+uint16_t posicao;
+uint16_t velocidade;
+uint16_t posicaoDesejada;	
+
 
 void setupStepper(void)
 {
-	// Not Implemented
+	posicaoAtual = 0;
+    velocidade = 50;
+	posicaoDesejada = 0;
+	
 }
 
 void setPosicaoAtual(uint16_t posicao_atual)
 {
-	// Not Implemented
+	posicaoAtual = posicao_atual;
+}
+
+void setVelocidade(uint16_t speed)
+{
+	velocidade = speed;
+}
+
+uint16_t getVelocidade()
+{
+	return velocidade;
 }
 
 uint16_t getPosicaoAtual(void)
 {
-	// Not Implemented
+	return posicaoAtual;
 }
 
 void setPosicaoDesejada(uint16_t posicao_desejada)
 {
-	// Not Implemented
+	posicaoDesejada = posicao_desejada;
+}
+
+void rotacionarParaDireita()
+{
+	float anguloStep = 11.25;
+	uint16_t steps = (int)(posicaoDesejada - posicaoAtual)/anguloStep;
 }
--- stepper.h
+++ stepper.h
@@ -8,8 +8,17 @@ void setupStepper(void);
 // Setta o valor da posição atual do motor
 void setPosicaoAtual(uint16_t posicao_atual);
 
+//Seta o valor da velocidade do motor
+void setVelocidade(uint16_t speed);
+
+//Retorna a velocidade do motor
+uint16_t getVelocidade()
+
 // Retorna o valor da posição atual do motor
 uint16_t getPosicaoAtual(void);
 
 // Setta a posição desejada
 void setPosicaoDesejada(uint16_t posicao_desejada);
+
+//Funçao para rotacionar o motor 
+void rotacionarParaDireita();
